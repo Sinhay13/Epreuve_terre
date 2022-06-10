d6c642211487e9bc17d91353b055778e6d42f007
Yahnis <yahnis.allegret@yahoo.com>
2022-06-10 16:46:53 +0900

    terre 12 mis en place du bon format pour traiter les données

--- terre012.py
+++ terre012.py
@@ -3,15 +3,19 @@ import sys
 
 def input_data():
     time = sys.argv[1:]
-    print(time)
+    return time
 
 
 def transform_data(time):
-    time_formated=""
-    time_formated=time[0]
-    list_time=time_formated.split(":")
-    return list_time
+    time_2=""
+    time_2=time[0]
+    list=[1,2,3]
+    list[0]=str(time_2[0])+str(time_2[1])
+    list[1]=str(time_2[2])+str(time_2[3])+str(time_2[4])
+    list[2]=str(time_2[5])+str(time_2[6])
+    return list
 
 
 
-input_data()
+time=input_data()
+list=transform_data(time)
