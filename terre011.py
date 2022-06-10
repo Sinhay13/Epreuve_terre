0a98dfd9dd614e8d8da41792fcab1c3ffba3a3e7
Yahnis <yahnis.allegret@yahoo.com>
2022-06-10 15:38:07 +0900

    Version final terre 11

--- terre011.py
+++ terre011.py
@@ -7,51 +7,59 @@ def input_data():
 
 
 def transform_data(time):
-    time.split(" ")
-    print(time)
+    time_formated=""
+    time_formated=time[0]
+    list_time=time_formated.split(":")
+    return list_time
 
 
-def time_12(time):
-    if time[0]==13:
-        time[0]=1
-        time.append("PM")
-    elif time[0]==14:
-        time[0]=2
-        time.append("PM")
-    elif time[0]==15:
-        time[0]=3
-        time.append("PM")
-    elif time[0]==16:
-        time[0]=4
-        time.append("PM")
-    elif time[0]==17:
-        time[0]=5
-        time.append("PM")
-    elif time[0]==18:
-        time[0]=6
-        time.append("PM")
-    elif time[0]==19:
-        time[0]=7
-        time.append("PM")
-    elif time[0]==20:
-        time[0]=8
-        time.append("PM")
-    elif time[0]==21:
-        time[0]=9
-        time.append("PM")
-    elif time[0]==22:
-        time[0]=10
-        time.append("PM")
-    elif time[0]==23:
-        time[0]=11
-        time.append("PM")
-    elif time[0]==24:
-        time[0]=00
-        time.append("AM")
+def time_12(list_time):
+    if list_time[0]=="13":
+        list_time[0]="1"
+        list_time.append("PM")
+    elif list_time[0]=="14":
+        list_time[0]="2"
+        list_time.append("PM")
+    elif list_time[0]=="15":
+        list_time[0]="3"
+        list_time.append("PM")
+    elif list_time[0]=="16":
+        list_time[0]="4"
+        list_time.append("PM")
+    elif list_time[0]=="17":
+        list_time[0]="5"
+        list_time.append("PM")
+    elif list_time[0]=="18":
+        list_time[0]="6"
+        list_time.append("PM")
+    elif list_time[0]=="19":
+        list_time[0]="7"
+        list_time.append("PM")
+    elif list_time[0]=="20":
+        list_time[0]="8"
+        list_time.append("PM")
+    elif list_time[0]=="21":
+        list_time[0]="9"
+        list_time.append("PM")
+    elif list_time[0]=="22":
+        list_time[0]="10"
+        list_time.append("PM")
+    elif list_time[0]=="23":
+        list_time[0]="11"
+        list_time.append("PM")
+    elif list_time[0]=="24":
+        list_time[0]="00"
+        list_time.append("AM")
     else:
-        time.append("AM")
-    print(time)
+        list_time.append("AM")
+    return list_time
+
+def new_time(time):
+    time.insert(1,":")
+    new_time="".join(time)
+    print(new_time)
 
 time=input_data()
-transform_data(time)
-#time_12(time)
+list_time=transform_data(time)
+list_time_2=time_12(list_time)
+new_time(list_time_2)
