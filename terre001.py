cea65532c23412031fc7cea58fa9693442bb51db
Yahnis <yahnis.allegret@yahoo.com>
2022-06-12 16:17:21 +0900

    terre 1 correction nouveau process

--- terre001.py
+++ terre001.py
@@ -1,3 +1,6 @@
+
+'''
+
 import __main__
 import os
 import sys
@@ -6,3 +9,16 @@ import sys
 full_name = os.path.basename(sys.argv[0])
 name = full_name.split(".")
 print(name[0])
+'''
+
+
+import sys
+
+
+def afficher():
+    arguments = sys.argv[0:]
+    r="".join(arguments)
+    print(r)
+
+
+afficher()
